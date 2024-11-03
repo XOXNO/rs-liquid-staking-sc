d54e4ff60ed2f259dd6ff495accb748bd130dc0e
Mihai Daniel Eremia <22687907+mihaieremia@users.noreply.github.com>
2024-11-03 04:42:48 +0200

    remove reserve rewards from LP math

--- liquid-staking/src/views.rs
+++ liquid-staking/src/views.rs
@@ -42,8 +42,7 @@ pub trait ViewsModule:
         // Exchange Rate = (Total EGLD in protocol / Total LS Supply) * PRECISION
         // This gives us how many atomic units of EGLD you get for 1 LS token
         // Example: If rate = 1.1 * 10^18, it means 1 LS token = 1.1 EGLD
-        (&storage_cache.virtual_egld_reserve + &storage_cache.rewards_reserve)
-            * BigUint::from(INITIAL_EXCHANGE_RATE)
+        &storage_cache.virtual_egld_reserve * &BigUint::from(INITIAL_EXCHANGE_RATE)
             / &storage_cache.ls_token_supply
     }
 
