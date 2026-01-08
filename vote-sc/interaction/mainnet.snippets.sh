d7714fb48c76a85803186f8f99fe1b7a897bc283
Mihai Daniel Eremia <me@dani.ro>
2026-01-08 22:20:00 +0400

    Update ROOT_HASH and PROPOSAL_ID in mainnet snippets; add repository guidelines documentation

--- vote-sc/interaction/mainnet.snippets.sh
+++ vote-sc/interaction/mainnet.snippets.sh
@@ -2,8 +2,8 @@ ADDRESS=erd1qqqqqqqqqqqqqpgqdnpmeseu3j5t7grds9dfj8ttt70pev66ah0sydkq9x
 PROXY=https://gateway.xoxno.com
 PROJECT="./output-docker/vote-sc/vote-sc.wasm"
 LIQUID_STAKING_SC_ADDRESS="erd1qqqqqqqqqqqqqpgq6uzdzy54wnesfnlaycxwymrn9texlnmyah0ssrfvk6"
-ROOT_HASH="0x1f1e38fabf5ca3461123799d0c8ff9ed784de008fa220ab056ac7be20889651c"
-PROPOSAL_ID=1
+ROOT_HASH="0x8d45b99f1b9ccb1eb5abb0c817fc160be792543dcbdbc53a6a2281b047e72ff2"
+PROPOSAL_ID=2
 
 deploy() {
     mxpy --verbose contract deploy --bytecode=${PROJECT} --recall-nonce \
@@ -28,9 +28,9 @@ setLiquidStakingAddress() {
 }
 
 setRootHash() {
-    mxpy contract call ${ADDRESS} --recall-nonce --function="set_root_hash" \
+    mxpy contract call ${ADDRESS} --function="set_root_hash" \
     --arguments ${ROOT_HASH} ${PROPOSAL_ID} \
-    --ledger --ledger-account-index=0 --ledger-address-index=0 \
+    --ledger \
     --gas-limit=50000000 --send --proxy=${PROXY} --chain=1 || return
 }
 
